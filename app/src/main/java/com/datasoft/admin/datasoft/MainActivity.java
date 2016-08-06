c8099441b7370dcee54d0b4f14c4bdb94854311e
pornsak30072 <pik30072@gmail.com>
2016-08-06 10:42:06 +0700

    Intent to SignUP finish

--- app/src/main/java/com/datasoft/admin/datasoft/MainActivity.java
+++ app/src/main/java/com/datasoft/admin/datasoft/MainActivity.java
@@ -1,7 +1,9 @@
 package com.datasoft.admin.datasoft;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //เอาเมาส์มาชี้แล้วคลิกจะไปที่ from
+    }   //Main Method
+
+    public void clickSignUpMain(View view) {
+        //ไปทำ onClick->clickSignUpMain ก่อน
+        //เคลื่อนย้าย MainActivity -> SignUpActivity
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+
+}   //Main Class
