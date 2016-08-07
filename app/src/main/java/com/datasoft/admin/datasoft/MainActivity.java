a81cc81290f557bd63e57e8c826f40b6ee70deb0
pornsak30072 <pik30072@gmail.com>
2016-08-07 11:54:06 +0700

    login String

--- app/src/main/java/com/datasoft/admin/datasoft/MainActivity.java
+++ app/src/main/java/com/datasoft/admin/datasoft/MainActivity.java
@@ -9,10 +9,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
         //Explicit โยนข้อมูลระหว่าง class
         private Context context;
-        private String myUserString, myPasswordString;
+        private String myUserString, myPasswordString, truePasswordString, nameString;
         private static final String urlJSON = "http://swiftcodingthai.com/6aug/get_user_datasoft.php";
+        private boolean statusABoolean = true;
 
         //Alt + insert = Constructor
         public SynchronizeUser(Context context,
@@ -75,6 +80,38 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(s);
 
             Log.d("7AugV1", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i+=1) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    if (myUserString.equals(jsonObject.getString("User"))) {
+
+                        truePasswordString = jsonObject.getString("Password");
+                        nameString = jsonObject.getString("Name");
+                        statusABoolean = false;
+
+                    }   //if
+                }   //for
+
+                if (statusABoolean) {
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, 4, "ไม่มี User นี้", "ไม่มี " + myUserString + "ใน ฐานข้อมูล");
+                } else if (passwordString.equals(truePasswordString)) {
+                    //Password True
+                    Toast.makeText(context, "Welcome " + nameString, Toast.LENGTH_SHORT).show();
+                } else {
+                    //Password False
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, 4, "Password False", "Please Try Again Password False");
+                }
+
+            } catch (Exception e) {
+                //เมื่อไหร่ที่ error ให้ทำการ print error
+                e.printStackTrace();
+            }
 
         }   //onPost
 
