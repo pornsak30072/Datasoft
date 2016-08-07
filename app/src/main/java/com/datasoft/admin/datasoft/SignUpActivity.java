38e3da3c49fdb19f012f5501de7426681a56335d
pornsak30072 <pik30072@gmail.com>
2016-08-07 09:52:57 +0700

    SignUp Success

--- app/src/main/java/com/datasoft/admin/datasoft/SignUpActivity.java
+++ app/src/main/java/com/datasoft/admin/datasoft/SignUpActivity.java
@@ -9,9 +9,15 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -123,7 +129,6 @@ public class SignUpActivity extends AppCompatActivity {
     private void uploadNewValueToServer() {
 
         OkHttpClient okHttpClient = new OkHttpClient();
-        //RequestBody requestBody = new FormEncodingBuilder();
         RequestBody requestBody = new FormEncodingBuilder()
                 .add("isAdd", "true")
                 .add("Name", nameString)
@@ -131,7 +136,23 @@ public class SignUpActivity extends AppCompatActivity {
                 .add("Password", passwordString)
                 .add("Avata", avataString)
                 .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder
+                .url("http://swiftcodingthai.com/6aug/add_user_datasoft.php")
+                .post(requestBody)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
 
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
 
     }   //uploadNewValue
 
