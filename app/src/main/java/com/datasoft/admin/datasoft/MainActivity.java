72fa36c77205b3411e194ad03b08cec205ffa7cd
pornsak30072 <pik30072@gmail.com>
2016-08-07 11:26:06 +0700

    JSON String

--- app/src/main/java/com/datasoft/admin/datasoft/MainActivity.java
+++ app/src/main/java/com/datasoft/admin/datasoft/MainActivity.java
@@ -1,10 +1,18 @@
 package com.datasoft.admin.datasoft;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +33,54 @@ public class MainActivity extends AppCompatActivity {
 
     }   //Main Method
 
+    //Create Inner Class
+    private class SynchronizeUser extends AsyncTask<Void, Void, String> {
+
+        //Explicit โยนข้อมูลระหว่าง class
+        private Context context;
+        private String myUserString, myPasswordString;
+        private static final String urlJSON = "http://swiftcodingthai.com/6aug/get_user_datasoft.php";
+
+        //Alt + insert = Constructor
+        public SynchronizeUser(Context context,
+                               String myUserString,
+                               String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+                //Exception = error ที่ยอมรับได้
+            }
+
+            //return null;
+        }   //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("7AugV1", "JSON ==> " + s);
+
+        }   //onPost
+
+    }   //SynUser Class
+
+
     public void clickSighIn(View view) {
 
         userString = userEditText.getText().toString().trim();
@@ -34,8 +90,11 @@ public class MainActivity extends AppCompatActivity {
         if (userString.equals("")  ||  passwordString.equals("")) {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, 3, "Have Space", "Please Fill All Every Blank");
-
         } else {
+
+            //Call SynUser
+            SynchronizeUser synchronizeUser = new SynchronizeUser(this, userString, passwordString);
+            synchronizeUser.execute();
 
         }
 
